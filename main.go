9226b5d28665671f2c822b2d57a35f142f3104b5
pauloabaia <paulinhus2@outlook.com>
2025-10-03 15:11:16 -0300

    feat: tratamento de erro nos scans

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	numeros := []int{1,2,3,4,5}
+	numeros := []int{}
 	var err error
 
 	for {
@@ -12,24 +12,31 @@ func main() {
 		fmt.Println("2 - Listar números")
 		fmt.Println("3 - Remover por índice")
 		fmt.Println("4 - Estatísticas: Mínimo, Máximo, Média")
-		fmt.Println("5 - Divisão por índice")
+		fmt.Println("5 - Divisão")
 		fmt.Println("6 - Limpar lista")
 		fmt.Println("0 - Sair")
 		fmt.Println()
 
 		var opcao int
 		fmt.Print("Escolha uma opção: ")
-		fmt.Scan(&opcao)
-
+		if _, scanErr := fmt.Scan(&opcao); scanErr != nil {
+			fmt.Println("Erro ao ler a opção.")
+			continue
+		}
 		switch opcao {
 
 		case 1:
 			fmt.Print("Digite um número para adicionar: ")
 			var valor int
-			fmt.Scan(&valor)
+			if _, scanErr := fmt.Scan(&valor); scanErr != nil {
+				fmt.Println("Erro ao ler o número.")
+				continue
+			}
 			numeros, err = adicionar(numeros, valor)
 			if err != nil {
 				fmt.Println("Erro:", err)
+			}else {
+				fmt.Println("Número adicionado.")
 			}
 			
 		
@@ -39,25 +46,44 @@ func main() {
 		case 3:
 			fmt.Print("Digite o índice do número para remover: ")
 			var indice int
-			fmt.Scan(&indice)
+			if _, scanErr := fmt.Scan(&indice); scanErr != nil{
+				fmt.Println("Digite um indice válido")
+				continue
+			}
+
 			numeros, err = remover(numeros, indice)
 			if err != nil {
 				fmt.Println("Erro:", err)
+			}else {
+				fmt.Println("Número removido.")
 			}
 
 		case 4:
-			estatisticas(numeros)
-
-		case 5:
-			var a int
-			var b int
-			fmt.Print("Digite o dividendo: ")
-			fmt.Scan(&a)
-			fmt.Print("Digite o divisor: ")
-			fmt.Scan(&b)
-			err = divisao(a, b)
+			min, max, media, err := estatisticas(numeros)
 			if err != nil {
 				fmt.Println("Erro:", err)
+			} else {
+				fmt.Printf("Resultado das estatísticas: Mínimo: %d, Máximo: %d, Média: %.2f\n", min, max, media)
+			}
+
+		case 5:
+			var a float64
+			var b float64
+			fmt.Print("Digite o dividendo: ")
+			if _, scanErr := fmt.Scan(&a); scanErr != nil {
+				fmt.Println("Digite um número válido.")
+				continue
+			}
+			fmt.Print("Digite o divisor: ")
+			if _, scanErr := fmt.Scan(&b); scanErr != nil {
+				fmt.Println("Digite um número válido.")
+				continue
+			}
+			result, err := divisao(a, b)
+			if err != nil {
+				fmt.Println("Erro:", err)
+			} else {
+       		 	fmt.Printf("Resultado da divisão: %.2f\n", result) 
 			}
 
 		case 6:
@@ -67,9 +93,6 @@ func main() {
 		case 0:
 			fmt.Println("Saindo...")
 			return
-		
-		default:
-			fmt.Println("Opção inválida. Tente novamente.")
 		}
 	}
 }
@@ -94,33 +117,35 @@ func remover(numeros []int, indice int) ([]int, error) {
 	return numeros, nil
 }
 
-func estatisticas(numeros []int) {
+func estatisticas(numeros []int) (int, int, float64, error) {
 	if len(numeros) == 0 {
-		fmt.Println("A lista está vazia.")
-		return
+		return 0, 0, 0, fmt.Errorf("A lista está vazia.")
 	}
 
 	min := numeros[0]
-	max := numeros[len(numeros)-1]
+	max := numeros[0]
 	count := 0
 	sum := 0
 
 	for i := 0; i < len(numeros); i++ {
 		count++
 		sum += numeros[i]
+		if numeros[i] < min {
+			min = numeros[i]
+		}
+		if numeros[i] > max {
+			max = numeros[i]
+		}
 	}
 	media := float64(sum) / float64(count)
-	fmt.Print("Estatísticas: ")
-	fmt.Printf("Mínimo: %d, Máximo: %d, Média: %.2f\n", min, max, media)
+	return min, max, media, nil
 }
 
-func divisao(a int, b int) (error) {
+func divisao(a float64, b float64) (float64, error) {
 	if b == 0 {
-		return fmt.Errorf("divisão por zero")
+		return 0, fmt.Errorf("Divisão por zero")
 	}
-	divisao := a / b
-	fmt.Print("Resultado da divisão: ", divisao, "\n")
-	return nil
+	return float64(a) / float64(b), nil
 }
 
 func limpar(numeros []int) []int {
